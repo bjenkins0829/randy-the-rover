8511b2f403f80e08fbf20c080a29db2667e47a92
Brandon <bjenkins0829@gmail.com>
2026-03-31 20:22:49 -0400

    Moving forward and backward is working based on the distance seen by the eyes

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,106 @@
 #include "Arduino.h"
 
-long microsecondsToInches(long microseconds);
 long getDistance();
-void tooClose();
 void setupEyes();
-void setupWheel();
+void setupWheels();
+void goForward();
+void goBackward();
+void stop();
 
-int trigPin = 7;
-int echoPin = 8;
-int motor1pin1 = 2;
-int motor1pin2 = 3;
-int speedMotor1 = 11;
+// eyes
+int trigPin = 12;
+int echoPin = 13;
+
+// left wheel
+int motor1pin1 = 9;
+int motor1pin2 = 8;
+int speedMotor1 = 10;
+
+// right wheel
 int motor2pin1 = 4;
 int motor2pin2 = 5;
-int speedMotor2 = 10;
+int speedMotor2 = 3;
+
+// motor direction
+bool goingForward = false;
+bool goingBackward = false;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
   setupEyes();
-  setupWheel();
+  setupWheels();
 }
 
 void loop() {
-  // long inches = getDistance();
+  Serial.println("beginning loop");
 
-  // Serial.print(inches);
-  // Serial.print("in");
-  // Serial.println();
+  if (getDistance() <= 7) {
+    goBackward();
+  } else {
+    goForward();
+  }
 
-  // if (inches <= 7) {
-  //   tooClose();
-  // }
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
 
-  analogWrite(speedMotor1, 50);
-  analogWrite(speedMotor2, 50);
+  delay(100);
+}
+
+/**
+ * Make both wheels go forward
+ */
+void goForward() {
+  if (goingBackward) {
+    stop();
+    delay(1000);
+  }
+
+  goingForward = true;
+  goingBackward = false;
 
   digitalWrite(motor1pin1, HIGH);
   digitalWrite(motor1pin2, LOW);
-  delay(1000);
 
   digitalWrite(motor2pin1, HIGH);
   digitalWrite(motor2pin2, LOW);
-  delay(1000);
+}
+
+/**
+ * Make both wheels go backward
+ */
+void goBackward() {
+  if (goingForward) {
+    stop();
+    delay(1000);
+  }
+
+  goingForward = false;
+  goingBackward = true;
 
   digitalWrite(motor1pin1, LOW);
   digitalWrite(motor1pin2, HIGH);
-  delay(1000);
 
   digitalWrite(motor2pin1, LOW);
   digitalWrite(motor2pin2, HIGH);
-  delay(1000);
-
-  delay(100);
-
 }
 
-void tooClose() {
-  Serial.println("TOO CLOSE RETARD BACK UP!!!!");
+/**
+ * Stop both wheels
+ */
+void stop() {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
 }
 
-void setupWheel() {
+/**
+ * Setup the wheel pin modes
+ */
+void setupWheels() {
   pinMode(motor1pin1, OUTPUT);
   pinMode(motor1pin2, OUTPUT);
   pinMode(motor2pin1, OUTPUT);
@@ -67,13 +108,23 @@ void setupWheel() {
 
   pinMode(speedMotor1, OUTPUT);
   pinMode(speedMotor2, OUTPUT);
+
+  analogWrite(speedMotor1, 120);
+  analogWrite(speedMotor2, 120);
 }
 
+/**
+ * Setup the eyes pin modes
+ */
 void setupEyes() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
+/**
+ * Get the distance of an object in front of the eyes (in inches)
+ * return distance in inches
+ */
 long getDistance() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -82,8 +133,4 @@ long getDistance() {
   digitalWrite(trigPin, LOW);
   long duration = pulseIn(echoPin, HIGH);
   return duration / 74 / 2;
-}
-
-long microsecondsToInches(long microseconds) {
-  return microseconds / 74 / 2;
 }
